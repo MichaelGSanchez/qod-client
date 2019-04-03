47386e178b0f53bb4265881cdf4a37cd8cdf88c3
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-04-02 20:44:32 -0600

    Extra Credit work to add in signout function and icon

--- app/src/main/java/edu/cnm/deepdive/qod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/qod/controller/MainActivity.java
@@ -3,16 +3,26 @@ package edu.cnm.deepdive.qod.controller;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.TextView;
+
+
 import edu.cnm.deepdive.qod.R;
 import edu.cnm.deepdive.qod.model.Source;
+import edu.cnm.deepdive.qod.service.GoogleSignInService;
 import edu.cnm.deepdive.qod.service.QodService.GetQodTask;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,10 +35,15 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+/*    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);*/
+
     quoteText = findViewById(R.id.quote_text);
     sourceName = findViewById(R.id.source_name);
     findViewById(R.id.answer_background).setOnClickListener((v) -> changeAnswer());
     listener = new ShakeListener();
+
+
   }
 
   @Override
@@ -114,6 +129,35 @@ public class MainActivity extends AppCompatActivity {
       // Do nothing.
     }
 
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.options_menu, menu);
+    return true;
+  }
+
+    @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    boolean handeled = true;
+      if (item.getItemId() == R.id.sign_out) {
+        signOut();
+      } else {
+        handeled = super.onOptionsItemSelected(item);
+      }
+    return handeled;
+  }
+
+  private void signOut() {
+    GoogleSignInService.getInstance().getClient()
+        .signOut()
+        .addOnCompleteListener(this, (task -> {
+          GoogleSignInService.getInstance().setAccount(null);
+          Intent intent = new Intent(this, LoginActivity.class);
+          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+        }));
   }
 
 }
